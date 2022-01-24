ca520f3b94fd33ecb2f7b9a9d6e058e6f7d356d9
Adebayo Samuel <eladebayoor@gmail.com>
2022-01-24 13:48:26 +0100

    made the item draggable

--- js/kanban.js
+++ js/kanban.js
@@ -5,6 +5,7 @@ const error = document.querySelector('.error');
 const message = 'Please add a description.';
 
 const add_btn = document.querySelector('.add');
+
 add_btn.addEventListener('click', () => {
   const target = document.querySelector('#requested');
   if (adding == false) {
@@ -16,6 +17,48 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+
+  const createElement = () => {
+    let item = document.createElement('div');
+    item.classList.add('item');
+    item.setAttribute("id", "item-");
+    item.innerHTML = "order";
+
+    function dragStart(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      item.style.top = (item.offsetTop - pos2) + "px";
+      item.style.left = (item.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+      // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+
+    item.onmousedown = dragStart;
+  }
+
+  createElement();
+
 };
 
 document.querySelectorAll('.drop').forEach(element => {
