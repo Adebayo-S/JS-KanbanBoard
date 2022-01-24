144674e40fad32abb4da1a4933dd68b5e01fdd7c
Adebayo Samuel <eladebayoor@gmail.com>
2022-01-24 14:34:02 +0100

    editing draggable attribute JS

--- js/kanban.js
+++ js/kanban.js
@@ -1,13 +1,15 @@
+"use strict";
+
 let order = 1;
 let adding = false;
 
-const error = document.querySelector('.error');
-const message = 'Please add a description.';
+const error = document.querySelector(".error");
+const message = "Please add a description.";
 
-const add_btn = document.querySelector('.add');
+const add_btn = document.querySelector(".add");
 
-add_btn.addEventListener('click', () => {
-  const target = document.querySelector('#requested');
+add_btn.addEventListener("click", () => {
+  const target = document.querySelector("#requested");
   if (adding == false) {
     adding = true;
     target.appendChild(create_item());
@@ -17,49 +19,20 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  let item = document.createElement("div");
+  item.classList.add("item");
+  item.setAttribute("id", "item-");
+  item.innerHTML = "order";
+  item.setAttribute("draggable", true);
 
-  const createElement = () => {
-    let item = document.createElement('div');
-    item.classList.add('item');
-    item.setAttribute("id", "item-");
-    item.innerHTML = "order";
+  item.addEventListener("dragstart", (e) => {
+    e.dataTransfer.setData("text", e.target.id);
+  });
 
-    function dragStart(e) {
-      e = e || window.event;
-      e.preventDefault();
-      // get the mouse cursor position at startup:
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      document.onmouseup = closeDragElement;
-      // call a function whenever the cursor moves:
-      document.onmousemove = elementDrag;
-    }
-
-    function elementDrag(e) {
-      e = e || window.event;
-      e.preventDefault();
-      // calculate the new cursor position:
-      pos1 = pos3 - e.clientX;
-      pos2 = pos4 - e.clientY;
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      // set the element's new position:
-      item.style.top = (item.offsetTop - pos2) + "px";
-      item.style.left = (item.offsetLeft - pos1) + "px";
-    }
-
-    function closeDragElement() {
-      // stop moving when mouse button is released:
-      document.onmouseup = null;
-      document.onmousemove = null;
-    }
-
-    item.onmousedown = dragStart;
-  }
-
-  createElement();
+  item.addEventListener("dragend", (e) => {
+    e.dataTransfer.clearData();
+  });
 
 };
 
-document.querySelectorAll('.drop').forEach(element => {
-});
+document.querySelectorAll(".drop").forEach((element) => {});
